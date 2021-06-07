965a98d62428cb2b9495efc33ee6a7ab63a238c1
Olga <o.smolyakova30@gmail.com>
2021-06-07 17:05:05 -0500

    added function and creted a playerName variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 function fight() {
-    window.alert("The fight has begun!")
+  window.alert("The fight has begun!"); 
 }
 
-fight();
+//fight();
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
